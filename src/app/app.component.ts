4c6ea5a549056c1ca32d3dbd264dec89cb04bcd2
DanielYang <ball4314@gmail.com>
2021-03-16 16:31:23 +0800

    single-spa導航欄

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-navbar',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  ngVersion = VERSION;
   title = 'navbar';
 }
